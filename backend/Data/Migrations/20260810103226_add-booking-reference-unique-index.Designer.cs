12a0d045178f431834ca0b857f51f241b96c3f3c
Helen Lee <helenheajilee@gmail.com>
2026-08-10 11:35:20 +0100

    add unique constraints to reference number

--- backend/Data/Migrations/20260810103226_add-booking-reference-unique-index.Designer.cs
+++ backend/Data/Migrations/20260810103226_add-booking-reference-unique-index.Designer.cs
@@ -4,6 +4,7 @@ using BookAStay.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace backend.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260810103226_add-booking-reference-unique-index")]
+    partial class addbookingreferenceuniqueindex
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
